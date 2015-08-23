71200e31ba0a89fcca34b5dc56356ecf24a0033e
fharr <florian.harreau@outlook.com>
2015-08-23 16:05:38 +0200

    Initial commit :
    -Creation of src and examples folder
    -Creation of the library first draft
    -Creation of a blank UnityExample

---
+++ src/SimpleDAO.EntityFramework/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿namespace SimpleDAO.EntityFramework
+{
+    using System;
+    using System.Data.Entity;
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        #region properties
+
+        public DbContext DbContext { get; private set; }
+
+        #endregion
+
+        #region constructors
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            this.DbContext = dbContext;
+        }
+
+        #endregion
+
+        #region methods
+
+        public void SaveChanges()
+        {
+            this.DbContext.SaveChanges();
+        }
+
+        #endregion
+
+        #region dispose
+
+        public void Dispose()
+        {
+            this.DbContext.Dispose();
+        }
+
+        #endregion
+    }
+}
---
+++ src/SimpleDAO/IGenericRepository.cs
@@ -0,0 +1,50 @@
+﻿namespace SimpleDAO
+{
+    using System.Collections.Generic;
+
+    public interface IGenericRepository<TDomain> where TDomain : class
+    {
+        /// <summary>
+        /// Creates a new object
+        /// </summary>
+        /// <param name="domain">the object to create</param>
+        void Create(TDomain domain);
+
+        /// <summary>
+        /// Gets an existing object by its id
+        /// </summary>
+        /// <param name="keyValues">the object's id</param>
+        /// <returns></returns>
+        TDomain GetById(params object[] keyValues);
+
+        /// <summary>
+        /// Gets all the object
+        /// </summary>
+        /// <returns></returns>
+        IList<TDomain> GetAll();
+
+        /// <summary>
+        /// Updates the specified object
+        /// </summary>
+        /// <param name="domain">the object to update</param>
+        void Update(TDomain domain);
+
+        /// <summary>
+        /// Deletes the specified object
+        /// </summary>
+        /// <param name="domain">the object to delete</param>
+        void Remove(TDomain domain);
+
+        /// <summary>
+        /// Deletes all the objects in the specified list
+        /// </summary>
+        /// <param name="list">the objects to delete</param>
+        void RemoveRange(IList<TDomain> list);
+
+        /// <summary>
+        /// Counts how many objects exist in the underlyning datastore
+        /// </summary>
+        /// <returns>the number of object</returns>
+        int Count();
+    }
+}
